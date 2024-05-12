3f14ce8ee45d7e408430619b1a1985c9b1ad6223
Avisror2 <avisrorofek@gmail.com>
2024-05-12 14:34:20 +0300

    Hangman Unit-9 + 10
    Added function to show the hidden word and they correct guesses in it show_hidden_word.
    
    Added an option to choose a word from a file using file path on user's pc.
    
    Added comments in all methods to make application more clear.
    
    Added main game loop functionality in the main method.
    
    Added a method to print the current stage of the game print_hangman.

--- hangman.py
+++ hangman.py
@@ -103,9 +103,9 @@ def check_valid_input(letter_guessed, old_letters_guessed):
     :return: The result whether the input is valid or not.
     :rtype: bool
     """
-    in_guessed = True
-    if not any(letter_guessed in char for char in old_letters_guessed):
-        in_guessed = False
+    in_guessed = True  # Set a new bool for checking
+    if not any(letter_guessed in char for char in old_letters_guessed):  # If the user guess wasn't in older guesses
+        in_guessed = False  # Update if the guess wasn't already guessed before
     return len(letter_guessed) == 1 and letter_guessed.isalpha() and not in_guessed
 
 
@@ -119,14 +119,16 @@ def try_update_letter_guessed(letter_guessed, old_letters_guessed):
     :return: The result whether the guess was added to the list or was already in it.
     :rtype: bool
     """
-    if check_valid_input(letter_guessed, old_letters_guessed):
+    # Check valid input
+    if check_valid_input(letter_guessed, old_letters_guessed) and letter_guessed not in old_letters_guessed:
         old_letters_guessed.append(letter_guessed)
         return True
-    print('X')
-    sorted_old_list = sorted(old_letters_guessed)
+    print('X')  # Input isn't valid
+    sorted_old_list = sorted(old_letters_guessed)  # Print the sorted guessed letters
     for char in sorted_old_list[:-1]:
         print("{} -> ".format(char), end='')
     print(sorted_old_list[-1])
+    return False
 
 
 def show_hidden_word(secret_word, old_letters_guessed):
@@ -139,13 +141,13 @@ def show_hidden_word(secret_word, old_letters_guessed):
     :return: Returns a string containing the current correct guesses of characters of the secret word.
     :rtype: string
     """
-    discovered_word = ''
-    for char in secret_word:
-        if char in old_letters_guessed:
-            discovered_word += '{} '.format(char)
-        else:
-            discovered_word += '_ '
-    return discovered_word
+    discovered_word = ''  # Initialize the new string of the discovered word
+    for char in secret_word:  # Iterate through the secret word
+        if char in old_letters_guessed:  # If the current character was guessed by the user
+            discovered_word += '{} '.format(char)  # Add the character to the correct location
+        else:  # If the character wasn't guessed by the user
+            discovered_word += '_ '  # Add '_' in the correct location instead
+    return discovered_word  # Returns a string of the discovered word
 
 
 def check_win(secret_word, old_letters_guessed):
@@ -158,15 +160,72 @@ def check_win(secret_word, old_letters_guessed):
     :return: Returns True if the user guessed the word correctly.
     :rtype: bool
     """
-    return all(char in old_letters_guessed for char in secret_word)
+    return all(char in old_letters_guessed for char in secret_word)  # Checks if all guesses are in secret word
 
 
 def print_hangman(num_of_tries):
-    print(HANGMAN_PHOTOS.get(num_of_tries))
+    """Prints the current stage of the game based on the number of tries the user tried guessing
+        the secret word.
+    param num_of_tries: the number of tries the user guessed a letter
+    :type num_of_tries: int
+    :return: Prints the current stage of the game based on the number of tries
+    :rtype: none
+    """
+    print(HANGMAN_PHOTOS.get(num_of_tries))  # Prints the current stage with stage art dictionary and key
+
+
+def choose_word(file_path, index):
+    """Receives a file path on the system and an index, and returns a tuple
+        containing the number of unique words in the word file along with the word in location index.
+    :param file_path: string representing the word file path on the system
+    :param index: index of word in word file
+    :type file_path: string
+    :type index: int
+    :return: Returns a tuple containing the number of unique words in the file, and the word in location index.
+    :rtype: tuple
+    """
+    with open(file_path, "r") as word_file:
+        words = word_file.read().split()  # Splits the file content into a list of words
+    unique_words = set(words)  # Creates a set of words to eliminate duplicates
+    unique_count = len(unique_words)  # Counts the number of unique words
+    if index < 1:  # Check if the index is less than 1
+        return unique_count, None
+    word_at_index = words[(index - 1) % len(words)]  # Retrieves the word at the specified index, wraps around if needed
+    return unique_count, word_at_index  # Returns a tuple with the count of unique words and the word at the index
 
 
 def main():
-    print('')
+    # Defining attributes
+    secret_word = ''  # Secret word string
+    old_letters_guessed = []  # List of all all user guesses
+    letter_guessed = ''  # Current user guess
+    num_of_tries = 1  # Current count of user guesses
+    first_round = True
+
+    print_starting_message()  # Starting message of the game
+    print_number_of_tries()
+
+    #  Start of game loop
+    try:
+        secret_word = choose_word(r"E:\words.txt", 17)[1]  # Check for errors in word file path and set the secret word
+    except Exception as e:
+        print("Word file path is incorrect: {}".format(e))
+
+    while num_of_tries < 6 and not check_win(secret_word, old_letters_guessed):  # Main game loop while no lose or win
+        print("Stage: {}".format(num_of_tries), end='')
+        print_hangman(num_of_tries)  # Print current hangman ASCII stage
+        print("Word: ", show_hidden_word(secret_word, old_letters_guessed))  # Print the hidden word guessed letters
+        letter_guessed = input("Enter your guess: ")
+
+        # Check if the word is in the secret word and user didn't already use the word
+        if try_update_letter_guessed(letter_guessed, old_letters_guessed) and (letter_guessed not in secret_word):
+            num_of_tries += 1
+
+    if num_of_tries > 5:  # Check win or lose condition and print message
+        print("You lost! The word was: {}".format(secret_word))
+        print_hangman(num_of_tries + 1)  # Print current hangman ASCII stage
+    else:
+        print("You won! The word was : {}".format(secret_word))
 
 
 if __name__ == '__main__':
